e7c7199fc59b51eccd7ce8950e2eac043542dba9
Asad Khan <asadkhan6192@gmail.com>
2024-09-08 14:05:26 +0500

    Write integration tests for AdminDashboard

---
+++ client/tests/integration/AdminDashboard.test.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import { expect, describe, it, vi } from "vitest";
+import AdminDashboard from "../../src/views/AdminDashboard";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { getFeedbackList } from "../../src/api/api";
+
+vi.mock("../../src/api/api", async () => {
+    const actual = await vi.importActual("../../src/api/api");
+
+    const getFeedbackList = vi.fn().mockReturnValue({
+        ok: true,
+        json: async () => ({ rows: [{ _id: "123", type: "negative", comments: "Hello", prompt: "A", response: "B", category: "other" }], hasMore: false, nextPage: 2 })
+    });
+
+
+    return {
+        ...actual,
+        getFeedbackList,
+    }
+});
+
+const client = new QueryClient();
+
+const renderComponent = () => render(<QueryClientProvider client={client}><AdminDashboard /></QueryClientProvider>);
+
+describe("AdminDashboard", () => {
+    it("renders correctly", async () => {
+        const { container } = renderComponent();
+
+        await waitFor(() => {
+            expect(container).toMatchSnapshot();
+            expect(getFeedbackList).toHaveBeenCalled();
+
+            expect(screen.getByText("Hello")).toBeInTheDocument();
+            expect(screen.getByText("A")).toBeInTheDocument();
+            expect(screen.getByText("B")).toBeInTheDocument();
+            expect(screen.getByText("other")).toBeInTheDocument();
+        })
+    });
+})
